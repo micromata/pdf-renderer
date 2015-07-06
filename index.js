2aa24709114d1b901efce30cdc6622fd45676037
Kai Dorschner <me@krnl.de>
2015-07-06 14:03:30 +0200

    Updated var name.

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import fs from 'fs';
 import handlebars from 'handlebars';
 import wkhtmltopdf from 'wkhtmltopdf';
 
-export default function render(templateFile, context = {}) {
+export default function render(templateFile, templateContext = {}) {
   return new Promise((resolve, reject) => {
     fs.readFile(templateFile, (err, contentBuffer) => {
       if (err) return reject(res.end(err));
       const template = handlebars.compile(contentBuffer.toString('utf8'));
-      console.log('Writing PDF with', context);
-      resolve(wkhtmltopdf(template(context)));
+      console.log('Writing PDF with', templateContext);
+      resolve(wkhtmltopdf(template(templateContext)));
     });
   });
 }
