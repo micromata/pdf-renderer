fff6cf249342e30eb1e07d236f1bce2a626987f6
Kai Dorschner <me@krnl.de>
2015-07-09 23:12:22 +0200

    Removed babel transpile reliance

--- index.es5.js
+++ index.es5.js
@@ -6,7 +6,7 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-exports.default = render;
+exports['default'] = render;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -23,17 +23,17 @@ var _wkhtmltopdf = require('wkhtmltopdf');
 var _wkhtmltopdf2 = _interopRequireDefault(_wkhtmltopdf);
 
 function render(templateFile) {
-  let templateContext = arguments[1] === undefined ? {} : arguments[1];
+  var templateContext = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
-  return new Promise((resolve, reject) => {
-    _fs2.default.readFile(templateFile, (err, contentBuffer) => {
+  return new Promise(function (resolve, reject) {
+    _fs2['default'].readFile(templateFile, function (err, contentBuffer) {
       if (err) return reject(res.end(err));
-      const template = _handlebars2.default.compile(contentBuffer.toString('utf8'));
+      var template = _handlebars2['default'].compile(contentBuffer.toString('utf8'));
       console.log('Writing PDF with', templateContext);
-      resolve((0, _wkhtmltopdf2.default)(template(templateContext)));
+      resolve((0, _wkhtmltopdf2['default'])(template(templateContext)));
     });
   });
 }
 
-module.exports = exports.default;
+module.exports = exports['default'];
 
